0d1e1a6f7d408a4c47c9856dff1c01363d15fc9a
XinyuRen2 <sc22xr@leeds.ac.uk>
2026-05-08 20:52:54 +0100

    Add basic crawler

--- src/crawler.py
+++ src/crawler.py
@@ -13,4 +13,15 @@ def fetch_page(url):
         return None
 
 html = fetch_page(BASE_URL)
-print(html[:500])
+
+soup = BeautifulSoup(html, "html.parser")
+
+quotes = soup.find_all("div", class_="quote")
+for quote in quotes:
+    text = quote.find("span", class_="text").get_text()
+    print(text)
+
+next_button = soup.find("li", class_="next")
+next_link = next_button.find("a")["href"]
+full_next_url = BASE_URL + next_link
+print(f"Next page URL: {full_next_url}")
