7c1705ddacd72dc22aec1d80f1b50a687bcd30b3
XinyuRen2 <sc22xr@leeds.ac.uk>
2026-05-09 15:00:36 +0100

    Craw more pages

--- src/crawler.py
+++ src/crawler.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 BASE_URL = "https://quotes.toscrape.com/"
+current_url = BASE_URL
 
 def fetch_page(url):
     try:
@@ -13,16 +14,26 @@ def fetch_page(url):
         print(f"Error fetching {url}: {e}")
         return None
 
-html = fetch_page(BASE_URL)
+while current_url:
 
-soup = BeautifulSoup(html, "html.parser")
+    print(f"Crawling: {current_url}")
 
-quotes = soup.find_all("div", class_="quote")
-for quote in quotes:
-    text = quote.find("span", class_="text").get_text()
-    print(text)
+    html = fetch_page(current_url)
 
-next_button = soup.find("li", class_="next")
-next_link = next_button.find("a")["href"]
-full_url = urljoin(BASE_URL, next_link)
-print(full_url)
+    soup = BeautifulSoup(html, "html.parser")
+
+    quotes = soup.find_all("div", class_="quote")
+    for quote in quotes:
+        text = quote.find("span", class_="text").get_text()
+        print(text)
+
+    next_exists = soup.find("li", class_="next")
+
+    if next_exists:
+        next_link = next_exists.find("a")["href"]
+        current_url = urljoin(BASE_URL, next_link)
+    else:
+        current_url = None
+
+    time.sleep(6)
+    
